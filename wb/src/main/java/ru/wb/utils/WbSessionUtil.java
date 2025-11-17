c09523d1937cb6185f8c9a2efb40cac39cffe9aa
kirill <liriescsgo@gmail.com>
2025-11-17 18:06:08 +0300

    F

--- wb/src/main/java/ru/wb/utils/WbSessionUtil.java
+++ wb/src/main/java/ru/wb/utils/WbSessionUtil.java
@@ -1,16 +1,16 @@
-package ru.wb.utils;
-
-import com.microsoft.playwright.BrowserContext;
-import lombok.extern.slf4j.Slf4j;
-
-
-@Slf4j
-public class WbSessionUtil {
-
-  public static boolean checkSessionAlive(BrowserContext ctx) {
-    return ctx.cookies().stream().anyMatch(c ->
-        c.domain.contains("wildberries.ru") &&
-            ("x_wbaas_token".equalsIgnoreCase(c.name) || c.name.startsWith("wbx-") || "_wbauid".equalsIgnoreCase(c.name))
-    );
-  }
-}
+//package ru.wb.utils;
+//
+//import com.microsoft.playwright.BrowserContext;
+//import lombok.extern.slf4j.Slf4j;
+//
+//
+//@Slf4j
+//public class WbSessionUtil {
+//
+//  public static boolean checkSessionAlive(BrowserContext ctx) {
+//    return ctx.cookies().stream().anyMatch(c ->
+//        c.domain.contains("wildberries.ru") &&
+//            ("x_wbaas_token".equalsIgnoreCase(c.name) || c.name.startsWith("wbx-") || "_wbauid".equalsIgnoreCase(c.name))
+//    );
+//  }
+//}
